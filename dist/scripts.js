96d3fa4d67102742c3a9132b59db818b40224fc7
Helena <helenasleterkesto@gmail.com>
2021-04-10 11:05:46 +0200

    Add script to js files. Initiated gulp js task and executed with no errors

---
+++ dist/scripts.js
@@ -0,0 +1,2 @@
+var testLocal="Here's a local script file!";console.log(testLocal),console.log("Here's another script file");
+//# sourceMappingURL=scripts.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const autoprefixer = require("autoprefixer");
 const combinemq = require("postcss-combine-media-query");
 const cssnano = require("cssnano");
 
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+
 // Sass tasks
 function scssTask() {
   return src("app/scss/style.scss", { sourcemaps: true })
@@ -14,4 +17,12 @@ function scssTask() {
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
-exports.default = scssTask;
+// Js tasks
+function jsTask() {
+  return src(["app/js/script.js", "app/js/script2.js"], { sourcemaps: true })
+    .pipe(concat("scripts.js"))
+    .pipe(uglify())
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+exports.default = series(scssTask, jsTask);
