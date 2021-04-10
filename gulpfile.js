b21702f31e36be261d0085f35b927c2a601f5e3f
Helena <helenasleterkesto@gmail.com>
2021-04-10 12:13:26 +0200

    Add gulp js tasks and executed with no errors

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ const cssnano = require("cssnano");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
 
 // Sass tasks
 function scssTask() {
@@ -18,13 +21,26 @@ function scssTask() {
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
+// browserify only take one file as parameter,
+// therefor concat js files first
+function concatJsTask() {
+  return src("app/js/*.js").pipe(concat("scripts.js")).pipe(dest("app/js"));
+}
+
+function browserifyTask() {
+  return browserify("app/js/scripts.js")
+    .bundle()
+    .pipe(source("scripts.js"))
+    .pipe(buffer())
+    .pipe(dest("app/js"));
+}
+
 // Js tasks
 function jsTask() {
-  return src(["app/js/script.js", "app/js/script2.js"], { sourcemaps: true })
-    .pipe(concat("scripts.js"))
+  return src("app/js/scripts.js", { sourcemaps: true })
     .pipe(babel({ presets: ["@babel/preset-env"] }))
     .pipe(uglify())
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
-exports.default = series(scssTask, jsTask);
+exports.default = series(scssTask, concatJsTask, browserifyTask, jsTask);
